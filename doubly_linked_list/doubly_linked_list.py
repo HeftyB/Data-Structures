608f3d687bc7a426e6d2f2bf795f9179c362cc51
Andrew Shields <andrewshields@Andrews-iMac.attlocal.net>
2020-08-12 02:05:52 -0400

    day 2 MVP

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -7,6 +7,21 @@ class ListNode:
         self.prev = prev
         self.value = value
         self.next = next
+
+    def get_value(self):
+        return self.value
+    
+    def get_prev(self):
+        return self.prev
+
+    def set_prev(self, prev=None):
+        self.prev = prev
+
+    def get_next(self):
+        return self.next
+
+    def set_next(self, next=None):
+        self.next = next
             
 """
 Our doubly-linked list class. It holds references to 
@@ -27,7 +42,13 @@ class DoublyLinkedList:
     the old head node's previous pointer accordingly.
     """
     def add_to_head(self, value):
-        pass
+        node = ListNode(value, None, self.head)
+        if self.head is not None:
+            self.head.set_prev(node)
+        if self.tail is None:
+            self.tail = node
+        self.head = node
+        self.length = self.length + 1
         
     """
     Removes the List's current head node, making the
@@ -35,7 +56,18 @@ class DoublyLinkedList:
     Returns the value of the removed Node.
     """
     def remove_from_head(self):
-        pass
+        returnval = self.head.get_value()
+        if self.head is None and self.tail is None:
+            return returnval
+        if self.head.get_next() == None:
+            self.head = None
+            self.tail = None
+            self.length = 0
+            return returnval
+        self.head = self.head.get_next()
+        self.head.set_prev()
+        self.length = self.length - 1
+        return returnval
             
     """
     Wraps the given value in a ListNode and inserts it 
@@ -43,7 +75,15 @@ class DoublyLinkedList:
     the old tail node's next pointer accordingly.
     """
     def add_to_tail(self, value):
-        pass
+        node = ListNode(value)
+        if self.head is None and self.tail is None:
+            self.head = node
+            self.tail = node
+        self.tail.set_next(node)
+        node.set_prev(self.tail)
+        self.tail = node
+        self.length = self.length + 1
+
             
     """
     Removes the List's current tail node, making the 
@@ -51,32 +91,79 @@ class DoublyLinkedList:
     Returns the value of the removed Node.
     """
     def remove_from_tail(self):
-        pass
+        returnval = self.tail.get_value()
+        if self.head is self.tail:
+            self.head = None
+            self.tail = None
+            self.length = 0
+            return returnval
+        self.tail.get_prev().set_next()
+        self.tail = self.tail.get_prev()
+        self.length = self.length - 1
+        return returnval
             
     """
     Removes the input node from its current spot in the 
     List and inserts it as the new head node of the List.
     """
     def move_to_front(self, node):
-        pass
+        node.get_prev().set_next(node.get_next())
+        if node.get_next() is not None:
+            node.get_next().set_prev(node.get_prev())
+        node.set_prev()
+        node.set_next(self.head)
+        self.head.set_prev(node)
+        self.head = node
+        
         
     """
     Removes the input node from its current spot in the 
     List and inserts it as the new tail node of the List.
     """
     def move_to_end(self, node):
-        pass
+        if node.get_prev() is not None:
+            node.get_prev().set_next(node.get_next)
+        if node.get_next() is not None:    
+            node.get_next().set_prev(node.get_prev)
+        if self.head is node:
+            self.head = node.get_next()
+        node.set_next()
+        node.set_prev(self.tail)
+        self.tail.set_next(node)
+        self.tail = node
 
     """
     Deletes the input node from the List, preserving the 
     order of the other elements of the List.
     """
     def delete(self, node):
-        pass
+        current = self.head
+        while current.get_next:
+            if current is node:
+                if current.get_prev() is not None:
+                    node.get_prev().set_next(node.get_next())
+                if self.head is node:
+                    self.head = node.get_next()
+                if current.get_next() is not None:
+                    node.get_next().set_prev(node.get_prev())
+                if self.tail is node:
+                    self.tail = node.get_prev()
+                node.set_next()
+                node.set_prev()
+                self.length = self.length - 1
+                break
+        current = current.get_next()
+        
 
     """
     Finds and returns the maximum value of all the nodes 
     in the List.
     """
     def get_max(self):
-        pass
+        current = self.head
+        max = self.head.get_value()
+        while current.get_next():
+            current = current.get_next()
+            if current.get_value() > max:
+                max = current.get_value()    
+        return max
--- queue/queue.py
+++ queue/queue.py
@@ -9,20 +9,30 @@ return elements in First In First Out order.
    Make sure the Queue tests pass.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Queue?
+
+   just like stack, syntax
    
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
 """
+from singly_linked_list import LinkedList
 class Queue:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        # self.storage = []
+        self.storage = LinkedList()
     
     def __len__(self):
-        pass
+        return self.size
 
     def enqueue(self, value):
-        pass
+        self.size = self.size + 1
+        # self.storage.append(value)
+        self.storage.add_to_tail(value)
 
     def dequeue(self):
-        pass
+        if self.size == 0:
+            return
+        self.size = self.size - 1
+        # return self.storage.pop(0)
+        return self.storage.remove_head()
